c87e066bce2ceea61fd7d03f3b5996f65e6e8ce1
Maty Carrion <mcsadevop@gmail.com>
2023-12-10 22:47:29 -0300

    Creacion username y token, ademas de primera respuesta del bot

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -7,8 +7,9 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
-    public static final String NAME = "agregue el nombre del bot entre comillas a continuación";
-    public static final String TOKEN = "agregue el token del bot entre comillas a continuación";
+    // https://core.telegram.org/bots/api link acceso a chat
+    public static final String NAME = "chatbetamcbot";
+    public static final String TOKEN = "6636133454:AAETWHTx39ehMvYeyOK6lPLz2XuMwOt7F7s";
 
     public MyFirstTelegramBot() {
         super(NAME, TOKEN);
@@ -17,6 +18,9 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         // TODO: escribiremos la funcionalidad principal del bot aquí
+
+        sendTextMessageAsync("¡ Hola ! \uD83D\uDC4B como estas ? \uD83D\uDE04 en que te puedo ayudar hoy \uD83E\uDD14 ? " );
+
     }
 
     public static void main(String[] args) throws TelegramApiException {
